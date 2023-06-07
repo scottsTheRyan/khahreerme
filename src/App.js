4da850f7b60fb2fe455d6f462a77b806046ea7f7
thedarkness <sthomas25854@protonmail.com>
2023-06-07 13:57:00 -0700

    toast functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Error, Landing, Register, Dashboard } from './pages';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='landing' element={<Landing />} />
-        <Route path='register' element={<Register />} />
-        <Route path='*' element={<Error />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
       </Routes>
+      <ToastContainer position='top-center' />
     </BrowserRouter>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import { Logo, FormRow } from "../components";
-
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { toast } from "react-toastify";
+
+
 
 
 const initialState = {
@@ -18,27 +20,39 @@ const Register = () => {
    // redux toolkit and useNavigate later
 
    const handleChange = (e) => {
-     console.log(e.target);
+     const name = e.target.name;
+     const value = e.target.value;
+    //  console.log(`${name}:${value}`);
+     setValues({ ...values, [name]: value });
    };
 
-   const onSubmit = (e) => {
-     e.preventDefault();
-     console.log(e.target);
-   };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast("You're the best, so fill out all fields");
+      
+    }
+  };
 
+   const toggleMember = () => {
+     setValues({ ...values, isMember: !values.isMember });
+   };
 
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
         {/* name field */}
-        <FormRow
-          type="text"
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
-        />
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
         {/* email field */}
         <FormRow
           type="email"
@@ -56,6 +70,13 @@ const Register = () => {
         <button type="submit" className="btn btn-block">
           submit
         </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
