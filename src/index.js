bc680b4fd5956b4e4f4aa349180a3e02f8ebe5db
thedarkness <sthomas25854@protonmail.com>
2023-06-07 12:58:18 -0700

    add login form rows

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "normalize.css";
+import "./index.css";
+import App from "./App";
 
-
-ReactDOM.render(
-  <div>
-    <App />
-  </div>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App tab="home" />);
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+   const [values, setValues] = useState(initialState);
 
-export default Register
+   // redux toolkit and useNavigate later
+
+   const handleChange = (e) => {
+     console.log(e.target);
+   };
+
+   const onSubmit = (e) => {
+     e.preventDefault();
+     console.log(e.target);
+   };
+
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name field */}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+}
+  
+export default Register;
